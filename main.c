21afa8cce3918437c1666ea14c6e0ef47c0814b8
Aaron Ogle <geekgonecrazy@users.noreply.github.com>
2015-10-29 16:58:52 -0500

    reverted names

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
-#pragma config(Motor, port3, leftWheel, tmotorVex393_MC29, openLoop)
-#pragma config(Motor, port4, rightWheel, tmotorVex393_MC29, openLoop, reversed)
-#pragma config(Motor, port5, handLeft, tmotorServoStandard, openLoop)
-#pragma config(Motor, port7, handRight, tmotorVex269_MC29, openLoop)
+#pragma config(Motor, port3, leftMotor, tmotorVex393_MC29, openLoop)
+#pragma config(Motor, port4, rightMotor, tmotorVex393_MC29, openLoop, reversed)
+#pragma config(Motor, port5, handServo, tmotorServoStandard, openLoop)
+#pragma config(Motor, port7, handServo2, tmotorVex269_MC29, openLoop)
 #pragma config(Motor, port8, handMotor, tmotorServoStandard, openLoop)
 #pragma config(Motor, port9, armMotor, tmotorVex269_MC29, openLoop)
 
@@ -26,17 +26,15 @@ task main ()
 	int  maxServoNeg = -127;
 	int  servoIncrement = 10;
 
-	motor[handLeft] = handState1;
-	motor[handRight] = handState2;
+	motor[handServo] = handState1;
+	motor[handServo2] = handState2;
 
 	wait1Msec(2000);
 
 	while(true) {
 
-		motor[leftWheel]  = vexRT[Ch3];   // Left Joystick Y value
-		motor[rightWheel] = vexRT[Ch2];   // Right Joystick Y value.
-		motor[armMotor] = 0;  // Arm motor
-		motor[handMotor] = 0;  // Hand motor
+		motor[leftMotor]  = vexRT[Ch3];   // Left Joystick Y value
+		motor[rightMotor] = vexRT[Ch2];   // Right Joystick Y value.
 
 		// Open hand
 		if (vexRT[Btn8D] == 1) {
@@ -48,8 +46,8 @@ task main ()
 
 			}
 
-			motor[handLeft] = handState1;
-			motor[handRight] = handState2;
+			motor[handServo] = handState1;
+			motor[handServo2] = handState2;
 
 		}
 
@@ -63,29 +61,39 @@ task main ()
 
 			}
 
-			motor[handLeft] = handState1;
-			motor[handRight] = handState2;
+			motor[handServo] = handState1;
+			motor[handServo2] = handState2;
 
 		}
 
-		// Button 5D raises
+		// Button 5D raises arm
 		if (vexRT[Btn5D] == 1) {
-			motor[armMotor] = 50;
+			motor[armMotor] = 100;
 		}
 
-		// Button 6D lowers
+		// Button 6D lowers arm
 		if (vexRT[Btn6D] == 1) {
-			motor[armMotor] = -50;
+			motor[armMotor] = -100;
 		}
 
-		// Button 5U rotates left
+		// If neither pressed stop motor
+		if (vexRT[Btn6D] == 0 && vexRT[Btn5u] == 0) {
+			motor[armMotor] = 0;
+		}
+
+		// Button 5U rotates hand left
 		if (vexRt[Btn5U] == 1) {
-			motor[handMotor] = 50;
+			motor[handMotor] = 100;
 		}
 
-		// Button 6U rotates right
+		// Button 6U rotates hand right
 		if (vexRt[Btn6U] == 1) {
-			motor[handMotor] = -50;
+			motor[handMotor] = -100;
+		}
+
+		// If neither pressed stop motor
+		if (vexRt[Btn5U] == 0 && vexRt[Btn6U] == 0) {
+			motor[handMotor] = 0;
 		}
 
 	}
